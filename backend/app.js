8d902870c7ed79c357332dbc92a33e46cdcd7db1
Niharika <niharika.202310@gmail.com>
2025-07-19 01:08:45 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { createServer } from "node:http";
-
+import dotenv from "dotenv";
+dotenv.config();
 import { Server } from "socket.io";
 
 import mongoose, { mongo } from "mongoose";
@@ -20,9 +21,7 @@ app.use(express.urlencoded({ extended: true, limit: "40kb" }));
 app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
-  const connectionDb = await mongoose.connect(
-    "mongodb+srv://niharika202310:ClHpLAE6lvJG80YH@cluster0.idqgek4.mongodb.net/videocall"
-  );
+  const connectionDb = await mongoose.connect(process.env.MONGODB_URI);
   console.log(`Connected to MongoDB : ${connectionDb.connection.host}`);
   server.listen(app.get("port"), () => {
     console.log("Server is running on port 8000");
